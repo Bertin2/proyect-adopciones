8cad423df948c4495adc2434498e76daf8dca1c3
Bertin2 <bertinvenancio3526.321@gmail.com>
2022-03-24 14:02:46 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-	host: "localhost",
-	user: "root",
-	password: "",
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "bffe824cd26b10",
+	password: "ddf8f9db",
 	port: 3306,
-	database: "adoption_system",
+	database: "heroku_f43ff55b6e92f6b",
 	secret_key:"V3nancio!"
 };
 exports.config = config;
