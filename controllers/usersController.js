1b29f94e9a26897f0d7ffc2d8a0b73d6c8f2bada
Bertin2 <bertinvenancio3526.321@gmail.com>
2022-03-24 19:56:55 -0600

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,7 +9,7 @@ controller.getLogin = (req,res) => {
         id: req.body.id,
         time: new Date().getTime()
     };
-    const token = jwt.sign({user}, db.secret_key, {expiresIn:'5m'});
+    const token = jwt.sign({user}, db.secret_key, {expiresIn:'10m'});
     res.status(200);
     format.success = true;
     format.code = 200;
