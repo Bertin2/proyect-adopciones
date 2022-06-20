0e7ce2b85cfef1e76c3893776f6faed1968e0668
Bertin2 <bertinvenancio3526.321@gmail.com>
2022-06-20 17:31:40 -0500

    Implementando error

--- validations/adoptionValidator.js
+++ validations/adoptionValidator.js
@@ -12,7 +12,7 @@ const generateIdValidators = () =>[
 const updateAdoptionValidators = () =>[
     check('id').notEmpty().isNumeric().withMessage("Invalid id"),
     check('user_id').notEmpty().isNumeric().withMessage("Invalid user id"),
-    check('pet_id').notEmpty().isNumeric().withMessage("Invalid pet id"),
+    check('pet_id').notEmpty().isNumeric().withMessage("Invalid pet id")
     check('date').notEmpty().isDate().withMessage("Invalid date")
 
 ]
